cc4e662175712650320357b6ecd4b7d66d07144a
Komal Raza <komal123088@gmail.com>
2026-04-24 10:34:13 +0500

    store

--- store/wishlistStore.ts
+++ store/wishlistStore.ts
@@ -14,7 +14,11 @@ export const useWishlistStore = create<WishlistStore>()(
     (set, get) => ({
       items: [],
       addItem: (productId) =>
-        set((state) => ({ items: [...new Set([...state.items, productId])] })),
+        set((state) => ({
+          items: state.items.includes(productId)
+            ? state.items
+            : [...state.items, productId],
+        })),
       removeItem: (productId) =>
         set((state) => ({
           items: state.items.filter((id) => id !== productId),
